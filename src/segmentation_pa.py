7716b52b736bbec054ad3a9b1abfad479acac328
paplessix <Gfentre2006b>
2020-12-18 16:06:38 +0100

    Add the possibility to create calls

--- src/segmentation_pa.py
+++ src/segmentation_pa.py
@@ -2,31 +2,22 @@ import librosa as lib
 import librosa.display
 import soundfile as sf
 from pathlib import Path
-
+from calls import signal_to_calls
 import numpy as np
+import sys
+import os
 import matplotlib.pyplot as plt
 
-#import warnings
-#warnings.filterwarnings('ignore')
+import warnings
+warnings.filterwarnings('ignore')
 
-ROOT_MP3 = "../mp3/"
-FILENAME = "Acrocephalus-arundinaceus-136005.mp3"
-FILENAME2 = 'Acrocephalus-schoenobaenus-120666.mp3'
+def get_slided_var(y,sr, kernel_size = 1000 ):
 
-y, sr = lib.load(Path(ROOT_MP3+FILENAME))
+    h = np.ones(2*kernel_size+1)/(kernel_size*2+1)
+    mean_signal = np.convolve(y, h, mode='same')
+    var = np.convolve(np.square(y - mean_signal), h, mode='same')
+    return var
 
-print('échantillons', len( y))
-print( 'sampling rate', sr)
-kernel_size = 1000
-h = np.ones(2*kernel_size+1)/(kernel_size*2+1)
-mean = np.convolve(y, h, mode='same')
-var = np.convolve( np.square(y-mean),h, mode='same')
-
-onset_env = librosa.onset.onset_strength(y=y, sr=sr*2, center = False)
-pulse = librosa.beat.plp(onset_envelope=onset_env, sr=sr)
-factor = len(y)//len(pulse)
-beats_plp = np.flatnonzero(librosa.util.localmax(pulse))*factor
-times = np.cumsum(1/sr*np.ones_like(y))
 
 def get_pulse(y, sr):
     onset_env = librosa.onset.onset_strength(y=y, sr=sr * 2, center=False)
@@ -35,37 +26,49 @@ def get_pulse(y, sr):
     beats_plp = np.flatnonzero(librosa.util.localmax(pulse)) * factor
     return beats_plp
 
+
 def get_optimal_sampling_size(pulse, mode ='mean'):
     modes = {"mean": np.mean, "min" : np.min, "max" : np.max}
     function = modes[mode]
     return round(function(pulse[1:]-pulse[:-1]))
 
-def sample_signal(y, sr, pulse,species_name):
+def save_call(call,sr, species_name,call_number, path ):
+    np.savetxt(path + f'call_{sr}_{species_name}_{call_number}.csv', call, delimiter=",")
+    print('SAVED : ',path +'call/'+ f'call_{sr}_{species_name}_{call_number}.csv' )
+def sample_pulse(y , sr , pulse, species_name , call_number ):
     size = get_optimal_sampling_size(pulse)
-    for index in pulse:
+    for i in range (len(pulse)):
+        index = pulse[i]
         sampled_pulse = y[index:index+size]
-        sf.write(f'{species_name}', y, sr)
-fig, ax = plt.subplots()
-ax.plot(times, y* ( var>= 0.001))
-final_pulse = []
-print(len(beats_plp))
-for i in beats_plp:
-    if (var>=0.001)[i]:
-        final_pulse.append(i)
-final_pulse = np.array(final_pulse)
-print(len(final_pulse))
-ax.vlines(times[final_pulse],0,1)
-ax.set(title='librosa.beat.plp', xlim=[10,20])
-plt.show()
-fig, ax = plt.subplots()
-ax.plot(times, y)
-ax.vlines(times[beats_plp],0,1)
-ax.set(title='librosa.beat.plp2', xlim=[20,30])
-plt.show()
+        sf.write(TARGETFILE+'pulse/'+f'pulse_{species_name}_{call_number}_{i}.mp3', sampled_pulse, sr)
 
-pulse_size = final_pulse[1:]-final_pulse[:-1]
-print(pulse_size*1/sr)
-print(np.mean(pulse_size) * 1/sr)
+def sample_signal(y, sr, species_name, path ):
+    call_indexes = signal_to_calls(y, calls = False)
+    for i in range(len(call_indexes)):
+        indexes = call_indexes[i]
+        call = y[indexes[0]:indexes[1]]
+        save_call(call, sr, species_name,i+1, path)
+def main():
+    ROOT_MP3 = '../mp3/'
+    DATAFILE = '../data/'
+    files = os.listdir(ROOT_MP3)
+    known_species = set(os.listdir(DATAFILE ))
+    for FILENAME in files:
+        y, sr = lib.load(Path(ROOT_MP3 + FILENAME))
+        species_name = FILENAME[:-11]
+        if not species_name in known_species:
+            os.mkdir('../data/'+species_name)
+        print(species_name)
+
+        TARGETFILE = DATAFILE+ species_name+'/'
+        sample_signal(y,sr, species_name, TARGETFILE)
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
 
 
 
