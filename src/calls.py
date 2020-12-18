5b714258add51bdac580f6b2109306ffd1aef831
BLeCorfec <bruno.le_corfec@mines-paristech.fr>
2020-12-18 15:33:53 +0100

    Corection de la fonction qui retourne les calls Ajout d'un paramètre pour choisir de retourner des couples d'indice ou des signaux

--- src/calls.py
+++ src/calls.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import librosa
 
-def signal_to_calls(signal):
+def signal_to_calls(signal, calls =True):
     """Retourne une liste de calls à partir du signal mis en entrée"""
 
     kernel_size = 1000
     h = np.ones(2*kernel_size+1)/(kernel_size*2+1)
     mean = np.convolve(signal, h, mode='same')
-    var = np.convolve( np.square(y-mean),h, mode='same')
+    var = np.convolve( np.square(signal - mean),h, mode='same')
 
-    var_seuil = np.array(var>0.0002)
+    var_seuil = np.array(var> 0.0002)
 
     kernel_size2 = 10000
     h2 = np.ones(2*kernel_size2+1)/(kernel_size2*2+1)
@@ -23,10 +23,14 @@ def signal_to_calls(signal):
     a = 0
     b = 1
     while b < n:
-        if liste[b] != liste[b-1] + 1:
-            l.append((liste[a] + 5000,liste[b-1] - 5000))
+        if sounds[b] != sounds[b-1] + 1:
+            indices_intervalles.append((sounds[a] + 5000,sounds[b-1] - 5000))
             a = b
         b+=1
-    l.append((liste[a] + 5000,liste[b-1] - 5000))
-    calls = [signal[a:b+1] for (a,b) in indices_intervalles]
-    return calls
+    indices_intervalles.append((sounds[a] + 5000,sounds[b-1] - 5000))
+
+    if calls :
+        calls = [signal[a:b+1] for (a,b) in indices_intervalles]
+        return calls
+
+    return indices_intervalles
